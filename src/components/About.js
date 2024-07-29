5b21349bc86655489a0d1683dfdb1504007950aa
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-07-29 20:52:57 +0300

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am going to ace this course for the next 4 remaining  months</p>
+    <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
